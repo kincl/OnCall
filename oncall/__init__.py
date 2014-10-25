364c64f286c42c4d673f9a642290dc8cd3b71a60
Jason Kincl <jkincl@gmail.com>
2014-10-25 15:10:29 -0400

    Only set no-cache for json responses

--- oncall/__init__.py
+++ oncall/__init__.py
@@ -38,11 +38,12 @@ def load_user(userid):
 @app.after_request
 def add_header(response):
     """
-    Do not cache the page
+    Do not cache json query responses
     """
-    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
+    if response.mimetype == 'application/json':
+        response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
     return response
 
 
